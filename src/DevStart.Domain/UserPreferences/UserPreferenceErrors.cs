22564c589002ea87d25f655fdbb767fa765414ec
VGA <109912901+VGA815@users.noreply.github.com>
2025-12-06 10:48:38 +0300

    Added errors for UserPreferences

---
+++ src/DevStart.Domain/UserPreferences/UserPreferenceErrors.cs
@@ -0,0 +1,11 @@
+﻿using DevStart.SharedKernel;
+
+namespace DevStart.Domain.UserPreferences
+{
+    public static class UserPreferenceErrors
+    {
+        public static Error NotFound(Guid userId) => Error.NotFound(
+            "UserPreferences.NotFound",
+            $"The user preferences with userId = '{userId}' was not found");
+    }
+}
