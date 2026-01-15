7d288bc1a671e2cbd87d28e816fdfb8b67a0bc3c
VGA <109912901+VGA815@users.noreply.github.com>
2026-01-15 19:28:28 +0300

    Several endpoints have been added

---
+++ src/DevStart.WebApi/Endpoints/Users/Login.cs
@@ -0,0 +1,32 @@
+﻿
+using DevStart.Application.Abstractions.Messaging;
+using DevStart.Application.Users.Login;
+using DevStart.SharedKernel;
+using DevStart.WebApi.Extensions;
+using DevStart.WebApi.Infrastructure;
+using System.Text.Json.Serialization;
+
+namespace DevStart.WebApi.Endpoints.Users
+{
+    internal sealed class Login : IEndpoint
+    {
+        public sealed record Request(
+            [property: JsonPropertyName("email")] string Email,
+            [property: JsonPropertyName("password")] string Password);
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("users/login", async (
+                Request request,
+                ICommandHandler<LoginUserCommand, string> handler,
+                CancellationToken cancellationToken) =>
+            {
+                var command = new LoginUserCommand(request.Email, request.Password);
+
+                Result<string> result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+                .WithTags(Tags.Users);
+        }
+    }
+}
