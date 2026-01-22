5d809e8f97572854f9ff1bec0d40584e63e2c34f
VGA <109912901+VGA815@users.noreply.github.com>
2026-01-22 23:09:51 +0300

    commit

---
+++ src/DevStart.WebApi/Endpoints/MediaFiles/Upload.cs
@@ -0,0 +1,34 @@
+﻿
+using DevStart.Application.Abstractions.Messaging;
+using DevStart.Application.MediaFiles.Upload;
+using DevStart.SharedKernel;
+using DevStart.WebApi.Extensions;
+using DevStart.WebApi.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace DevStart.WebApi.Endpoints.MediaFiles
+{
+    internal sealed class Upload : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("files/media", async (IFormFile file, [FromQuery] Guid ownerId, ICommandHandler<UploadMediaFileCommand, Guid> handler, CancellationToken cancellationToken) =>
+            {
+                await using var stream = file.OpenReadStream();
+
+                var command = new UploadMediaFileCommand(
+                    ownerId,
+                    stream,
+                    file.ContentType,
+                    file.Length);
+
+                Result<Guid> result = await handler.Handle(command, cancellationToken);
+
+                return result.Match(Results.Ok, CustomResults.Problem);
+            })
+                .RequireAuthorization()
+                .WithTags(Tags.MediaFiles);
+        }
+    }
+}
