fc0e7b75eeb4f54852b9451b33305ddd7c670aa6
VGA <109912901+VGA815@users.noreply.github.com>
2026-01-22 23:08:32 +0300

    media file upload use case added

---
+++ src/DevStart.Application/MediaFiles/Upload/UploadMediaFileCommand.cs
@@ -0,0 +1,20 @@
+﻿using DevStart.Application.Abstractions.Messaging;
+
+namespace DevStart.Application.MediaFiles.Upload
+{
+    public sealed class UploadMediaFileCommand : ICommand<Guid>
+    {
+        public Guid OwnerId { get; set; }
+        public Stream FileStream { get; set; } = null!;
+        public string ContentType { get; set; } = null!;
+        public long Size { get; set; }
+
+        public UploadMediaFileCommand(Guid ownerId, Stream fileStream, string contentType, long size)
+        {
+            OwnerId = ownerId;
+            FileStream = fileStream;
+            ContentType = contentType;
+            Size = size;
+        }
+    }
+}
---
+++ src/DevStart.Application/MediaFiles/Upload/UploadMediaFileCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using DevStart.Application.Abstractions.Authentication;
+using DevStart.Application.Abstractions.Data;
+using DevStart.Application.Abstractions.Messaging;
+using DevStart.Domain.MediaFiles;
+using DevStart.Domain.Users;
+using DevStart.SharedKernel;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevStart.Application.MediaFiles.Upload
+{
+    internal sealed class UploadMediaFileCommandHandler(IApplicationDbContext context, IUserContext userContext, IFileStorage fileStorage, IDateTimeProvider dateTimeProvider)
+        : ICommandHandler<UploadMediaFileCommand, Guid>
+    {
+        public async Task<Result<Guid>> Handle(UploadMediaFileCommand command, CancellationToken cancellationToken)
+        {
+            if (!await context.Users.AnyAsync(u => u.Id == command.OwnerId && u.Id == userContext.UserId))
+            {
+                return Result.Failure<Guid>(UserErrors.NotFound(command.OwnerId));
+            }
+
+            MediaFile mediaFile = new MediaFile()
+            {
+                FileSize = (int)command.Size,
+                FileType = MediaFileType.Img,
+                FileUrl = "",
+                Id = Guid.NewGuid(),
+                Name = $"USERID_{userContext.UserId}_MEDIAFILE",
+                UploadDate = dateTimeProvider.UtcNow,
+                UploaderId = userContext.UserId
+            };
+
+            var objectKey = $"files/{mediaFile.Id}";
+
+            await fileStorage.UploadAsync(
+                objectKey,
+                command.FileStream,
+                command.ContentType,
+                cancellationToken);
+
+            context.MediaFiles.Add(mediaFile);
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return mediaFile.Id;
+        }
+    }
+}
