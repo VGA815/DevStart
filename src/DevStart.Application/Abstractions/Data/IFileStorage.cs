0e76007cdc03fbf91dc71cf84c87532fe42eeb58
VGA <109912901+VGA815@users.noreply.github.com>
2026-01-22 23:08:07 +0300

    file storage interface added

---
+++ src/DevStart.Application/Abstractions/Data/IFileStorage.cs
@@ -0,0 +1,20 @@
+﻿namespace DevStart.Application.Abstractions.Data
+{
+    public interface IFileStorage
+    {
+        Task UploadAsync(
+            string objectKey,
+            Stream data,
+            string contentType,
+            CancellationToken cancellationToken = default);
+
+        Task<string> GetPresignedUrlAsync(
+            string objectKey,
+            TimeSpan expiresIn,
+            CancellationToken cancellationToken= default);
+
+        Task DeleteAsync(
+            string objectKey,
+            CancellationToken cancellationToken = default);
+    }
+}
