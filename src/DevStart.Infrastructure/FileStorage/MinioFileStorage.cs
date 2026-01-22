f9be229b586fb5949aa45322b83f723d8641c93c
VGA <109912901+VGA815@users.noreply.github.com>
2026-01-22 23:08:56 +0300

    minio implemented

---
+++ src/DevStart.Infrastructure/FileStorage/MinioFileStorage.cs
@@ -0,0 +1,39 @@
+﻿using DevStart.Application.Abstractions.Data;
+using Minio;
+using Minio.DataModel.Args;
+
+namespace DevStart.Infrastructure.FileStorage
+{
+    public class MinioFileStorage(MinioClient minioClient, string bucket) : IFileStorage
+    {
+        public async Task DeleteAsync(string objectKey, CancellationToken cancellationToken = default)
+        {
+            await minioClient.RemoveObjectAsync(
+                new RemoveObjectArgs()
+                    .WithBucket(bucket)
+                    .WithObject(objectKey),
+                    cancellationToken);
+        }
+
+        public async Task<string> GetPresignedUrlAsync(string objectKey, TimeSpan expiresIn, CancellationToken cancellationToken = default)
+        {
+            return await minioClient.PresignedGetObjectAsync(
+                new PresignedGetObjectArgs()
+                    .WithBucket(bucket)
+                    .WithObject(objectKey)
+                    .WithExpiry((int)expiresIn.TotalSeconds));
+        }
+
+        public async Task UploadAsync(string objectKey, Stream data, string contentType, CancellationToken cancellationToken = default)
+        {
+            await minioClient.PutObjectAsync(
+                new PutObjectArgs()
+                    .WithBucket(bucket)
+                    .WithObject(objectKey)
+                    .WithStreamData(data)
+                    .WithObjectSize(data.Length)
+                    .WithContentType(contentType),
+                    cancellationToken);
+        }
+    }
+}
