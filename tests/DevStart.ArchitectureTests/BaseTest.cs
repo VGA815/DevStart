43744882c73e37409553855c850faaabcb4a257c
VGA <109912901+VGA815@users.noreply.github.com>
2026-02-24 18:18:10 +0300

    Add architecture tests to enforce clean layer boundaries

---
+++ tests/DevStart.ArchitectureTests/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using DevStart.Application.Abstractions.Messaging;
+using DevStart.Domain.Users;
+using DevStart.Infrastructure.Database;
+using DevStart.WebApi;
+using System.Reflection;
+
+namespace DevStart.ArchitectureTests
+{
+    public abstract class BaseTest
+    {
+        protected static readonly Assembly DomainAssembly = typeof(User).Assembly;
+        protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+        protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+        protected static readonly Assembly PresentationAssembly = typeof(Program).Assembly;
+    }
+}
